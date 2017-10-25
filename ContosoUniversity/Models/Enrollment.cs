435f7198c3e0573a5a5ca6e59b07eea2bfe58552
Kevin_Gillispie <oakenquill@gmail.com>
2017-10-24 19:54:21 -0700

    Add project files.

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }   // Primary Key
+        public int CourseID { get; set; }       // Foreign Key
+        public int StudentID { get; set; }      // Foreign Key
+        public Grade? Grade { get; set; }       // enum. The question mark after the Grade type declaration indicates that the Grade property is nullable.
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
